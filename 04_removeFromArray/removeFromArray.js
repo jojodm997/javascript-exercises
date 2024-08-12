546044cf428fef40039aeb0163d8594aeeb365dc
Fauzan <fauzandwimulyono@gmail.com>
2024-08-12 16:43:21 +0700

    Solved exe4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr, ...args) {
+  return arr.filter(val => !args.includes(val));
+}
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
