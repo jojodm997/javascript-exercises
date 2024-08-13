3a886d21553381ccd625d1c51a24b3faffa5ebd6
Fauzan <fauzandwimulyono@gmail.com>
2024-08-13 19:50:49 +0700

    Solved 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const ifYearDivisibleByFour = year % 4 === 0;
+  const ifYearDivisbleByOneHundred = year % 100 === 0;
+  const ifYearDivisibleByFourHundred = year % 400 === 0;
 
+  if (ifYearDivisibleByFour && (!ifYearDivisbleByOneHundred || ifYearDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
