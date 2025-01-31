19a76cfa6b8509afdb76494469507413728ed2a9
Fauzan <fauzandwimulyono@gmail.com>
2025-01-31 18:07:58 +0700

    Solved all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+  if (!Number.isInteger(begin) || !Number.isInteger(end)) return "ERROR";
+  if (begin < 0) return "ERROR";
+  if (begin > end) [begin, end] = [end, begin];
 
+  let result = 0;
+  for (let i = begin; i <= end; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
