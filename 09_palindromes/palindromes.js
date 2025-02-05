431d220de6707fafaf38aa2f865c783ffa9ba56c
Fauzan <fauzandwimulyono@gmail.com>
2025-02-05 20:48:32 +0700

    Solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function(word) {
+  const alpha = "abcdefghizklmnopqrstuvwxyz123456789"
+  let cleanedWord = word.toLowerCase().split("").filter(x => alpha.includes(x)).join("")
+  let reversedWord = cleanedWord.split("").reverse().join("") 
+  
+  return cleanedWord === reversedWord
 };
 
 // Do not edit below this line
