3a2e7536d1333cc87fa4f668dcc114e089b9d6c1
Fauzan <fauzandwimulyono@gmail.com>
2025-02-05 21:35:52 +0700

    Change from isInteger to type of

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(begin, end) {
-  if (!Number.isInteger(begin) || !Number.isInteger(end)) return "ERROR";
+  if (typeof begin !== "number" || typeof end !== "number") return "ERROR";
   if (begin < 0) return "ERROR";
   if (begin > end) [begin, end] = [end, begin];
 
