15b54b32c0989c914f60ff20e2ee1f3dbe85efab
Fauzan <fauzandwimulyono@gmail.com>
2025-02-23 12:28:35 +0700

    Solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  f = [0, 1];
 
+  for (let n = 2; n <= num; n++) {
+    f[n] = f[n - 1] + f[n - 2]
+  }
+  return f[num];
 };
 
 // Do not edit below this line
