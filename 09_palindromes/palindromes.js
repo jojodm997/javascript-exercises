bf5edd1b7a0680d33fc9ae8ca4fc9c587d82839a
Fauzan <fauzandwimulyono@gmail.com>
2025-02-23 12:28:53 +0700

    Fix

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
 const palindromes = function(word) {
   const alpha = "abcdefghizklmnopqrstuvwxyz123456789"
-  let cleanedWord = word.toLowerCase().split("").filter(x => alpha.includes(x)).join("")
+  let cleanedWord = word.toLowerCase()
+                        .split("")
+                        .filter(x => alpha.includes(x))
+                        .join("")
+
   let reversedWord = cleanedWord.split("").reverse().join("") 
   
   return cleanedWord === reversedWord
